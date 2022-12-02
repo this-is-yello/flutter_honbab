6d7089472885c3590c9700db4d8c4c4ac76e5f20
this-is-yello <yello_3617@naver.com>
2022-12-03 03:23:44 +0900

    221203

--- flutter_honbab_app/lib/main.dart
+++ flutter_honbab_app/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_honbab_app/screens/home_screen.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: MyApp(),
-  ) 
-);
+void main() => runApp(MaterialApp(
+      theme: style,
+      home: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,10 +14,9 @@ class MyApp extends StatelessWidget {
     return DefaultTabController(
       length: 1,
       child: Scaffold(
-          appBar: AppBar(
-          title: Text('현식'),
+        appBar: AppBar(
+          title: Center(child: Text('현식')),
         ),
-
         body: TabBarView(
           children: [
             Center(
@@ -26,24 +24,29 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
-        
         bottomNavigationBar: Container(
           height: 56,
           color: Colors.white,
           child: const TabBar(
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicatorColor: Color(0xff99001C),
-            labelColor: Color(0xff99001C),
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home_outlined),
-                text: 'home',
-              ),
-            ]
-          ),
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorColor: Color(0xff99001C),
+              labelColor: Color(0xff99001C),
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home_outlined),
+                  text: 'home',
+                ),
+              ]),
         ),
       ),
     );
   }
 }
+
+var style = ThemeData(
+    appBarTheme: AppBarTheme(
+  backgroundColor: Colors.white,
+  titleTextStyle: TextStyle(
+      color: Color(0xff99001C), fontSize: 20, fontWeight: FontWeight.w900),
+));
